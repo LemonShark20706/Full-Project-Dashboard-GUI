4fd292355bcb14f4434b4f48192276635ed01ae0
Oláh <adam.olah06@gmail.com>
2026-02-23 16:33:05 +0100

    Main Journal: Update tickets and labors from Python to JavaScript

--- main.py
+++ main.py
@@ -8,5 +8,13 @@ def hello_from_python(name):
     print(f"Python: Helló, {name}!")
     return f"Szia {name}, üdv Pythonból!"
 
+def notifywith_tickets_js(value):
+    eel.updateTicketsFromPython(value)
+
+def notifywith_labors_js(value):
+    eel.updateLaborsFromPython(value)
+
+notifywith_tickets_js(["2026-01-10", "2027-01-10", "2028-01-10"])
+notifywith_labors_js(["Labor 1", "Labor 2", "Labor 3"])
 
 eel.start('index.html', size=(800, 600))#, cmdline_args=[ '--start-fullscreen' ]
--- web/src/js/script.js
+++ web/src/js/script.js
@@ -6,6 +6,35 @@ function sendToPython() {
     });
 }
 
+eel.expose(updateTicketsFromPython);
+function updateTicketsFromPython(value) {
+    const days = value;
+
+    const dropdown = document.getElementById("ticketDropdown");
+    dropdown.innerHTML = "";
+
+    days.forEach(day => {
+        const li = document.createElement("li");
+        li.innerHTML = `<a class="dropdown-item" href="#">Ticker ${day}</a>`;
+        dropdown.appendChild(li);
+    });
+}
+
+
+eel.expose(updateLaborsFromPython);
+function updateLaborsFromPython(value) {
+    const days = value;
+
+    const dropdown = document.getElementById("laborDropdown");
+    dropdown.innerHTML = "";
+
+    days.forEach(labor => {
+        const li = document.createElement("li");
+        li.innerHTML = `<a class="dropdown-item" href="#">${labor}</a>`;
+        dropdown.appendChild(li);
+    });
+}
+
 function goFull() {
     document.documentElement.requestFullscreen();
 }
